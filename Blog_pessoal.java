35ee60b91f23331bddf5b9db63a70a4b2fc3f275
Wytheny <wtmarinho55@gmail.com>
2023-05-22 11:45:45 -0300

    início do projeto

---
+++ Blog_pessoal.java
@@ -0,0 +1,21 @@
+public static int create_post (){
+
+}
+
+public static int read_post (){
+
+}
+
+public static int update_post (){
+
+}
+
+public static int delete_post (){
+
+}
+
+public class Blog_pessoal {
+    public static void main(String[] args) {
+        
+    }
+}
