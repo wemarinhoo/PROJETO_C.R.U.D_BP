d504d44366247ad9405d1ee795db35383e5fe1fd
Wytheny <wtmarinho55@gmail.com>
2023-05-24 20:15:51 -0300

    BlogPessoal-V2

--- Blog_pessoal.java
+++ Blog_pessoal.java
@@ -1,75 +1,69 @@
-import java.sql.Date;
+import java.util.Date;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/** Códigos 
- * 0 = OK
- * 1 = Erro
- * 2 = Id já utilizado por uma postagem
- * */
+ public class Blog_pessoal {
 
-public class Blog_pessoal {
+  public static ArrayList<Integer> ids = new ArrayList<>();
+  public static ArrayList<String> nomes = new ArrayList<>();
+  public static ArrayList<String> categorias = new ArrayList<>();
+  public static ArrayList<String> titulos = new ArrayList<>();
+  public static ArrayList<String> conteudos = new ArrayList<>();
+  public static ArrayList<Date> datas = new ArrayList<>();
 
-    public static ArrayList<Integer> ids = new ArrayList<>();
-    public static ArrayList<String> nomes = new ArrayList<>();
-    public static ArrayList<String> temas = new ArrayList<>();
-    public static ArrayList<String> titulos = new ArrayList<>();
-    public static ArrayList<String> conteudos = new ArrayList<>();
-    public static ArrayList<Date> datas = new ArrayList<>();
+  public static int create_post (Integer id, String nome, String categoria, String titulo, String conteudo, Date data){
 
-    public static int create_post (Integer id, String nome, String tema, String titulo, String conteudo, Date data){
+      int postagem_existe = read_post(id);
 
-        int postagem_existe = read_post(id);
+      if(postagem_existe >= 1){
+              return 2;
+      }
 
-        if(postagem_existe >= 2){
-                return 2;
-        }
+      ids.add(id);
+      nomes.add(nome);
+      categorias.add(categoria);
+      titulos.add(titulo);
+      conteudos.add(conteudo);
+      datas.add(data);
 
-        ids.add(id);
-        nomes.add(nome);
-        titulos.add(titulo);
-        conteudos.add(conteudo);
-        datas.add(data);
+      return 0;
 
-        return 0;
+  }
 
-    }
+  public static int read_post (Integer id, String tema, Date data){
+      
 
-    public static int read_post (Integer id){
-        
+  }
 
-    }
+  public static int update_post (){
 
-    public static int update_post (){
+  }
 
-    }
+  public static int delete_post (){
 
-    public static int delete_post (){
+  }
+  public static void main(String[] args) {
 
-    }
-    public static void main(String[] args) {
+      int id = 1, operacao;
 
-        int id = 1, operacao;
+      Scanner scan = new Scanner(System.in);
 
-        Scanner scan = new Scanner(System.in);
+      while(true){
+          System.out.println("Selecione o Número Que Representa a Operação Desejada:");
+          System.out.println("1 - Criar Postagem");
+          System.out.println("2 - Buscar Postagem");
+          System.out.println("3 - Atualizar Postagem");
+          System.out.println("4 - Deletar Postagem");
+          System.out.println("5 - Listar Todas as Postagens");
+          System.out.println("0 - Sair");
+          System.out.println("-----------------------------------");
+          operacao = scan.nextInt();
 
-        while(true){
-            System.out.println("Selecione o Número Que Representa a Operação Desejada:");
-            System.out.println("1 - Criar Postagem");
-            System.out.println("2 - Buscar Postagem");
-            System.out.println("3 - Atualizar Postagem");
-            System.out.println("4 - Deletar Postagem");
-            System.out.println("5 - Listar Todas as Postagens");
-            System.out.println("0 - Sair");
-            operacao = scan.nextInt();
+          if(operacao == 1){
 
-            if(operacao == 1){
+          }
+      }
 
-            }
-        }
-
-        
-
-        scan.close();
-    }
+      scan.close();
+  }
 }
